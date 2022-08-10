2bef5e4a0e83fce0ed0e963cb87f02ff2dc4ec76
Justice Johnson <79010796+justlyJustice@users.noreply.github.com>
2022-08-10 14:18:41 +0100

    commiting file changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,67 @@
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image
+          alt=""
+          src={`/images/bg.png`}
+          layout={`fill`}
+          objectFit={`cover`}
+        />
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE DELIVER... THE BOUNGAI WAY
+          </h2>
+        </div>
+
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            Sonia Estate
+            <br /> Amarata,
+            <br /> Yenagoa, Bayelsa State.
+          </p>
+
+          <p className={styles.text}>
+            Sonia Estate
+            <br /> Amarata,
+            <br /> Yenagoa, Bayelsa State.
+          </p>
+
+          <p className={styles.text}>
+            Sonia Estate
+            <br /> Amarata,
+            <br /> Yenagoa, Bayelsa State.
+          </p>
+
+          <p className={styles.text}>
+            Sonia Estate
+            <br /> Amarata,
+            <br /> Yenagoa, Bayelsa State.
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 - 22:00
+          </p>
+
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 - 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
